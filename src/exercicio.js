b07b5ab402709ffb10eece69cb66bdb15cd81aa3
mateusu <alves.mate@hotmail.com>
2025-04-02 17:12:18 -0300

    Adiciona funções de multiplicação e saudação em TypeScript

---
+++ src/exercicio.js
@@ -0,0 +1,2 @@
+var multiplicacao = function (num1, num2) { return num1 * num2; };
+var saudacao = function (nome) { return "Ol\u00E1 ".concat(nome); };
---
+++ src/exercicio.ts
@@ -0,0 +1,3 @@
+
+const multiplicacao = (num1: number, num2: number): number => num1 * num2;
+const saudacao = (nome: string): string => `Olá ${nome}`;
